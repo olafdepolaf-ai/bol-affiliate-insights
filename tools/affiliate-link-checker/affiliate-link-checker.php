c6e5f3639ef3bdcae2fbef9a4b600f4a43b8561e
OlafL <github@lemmers.net>
2026-05-20 00:09:43 +0200

    chore(alc): bump version to 0.2.8

--- tools/affiliate-link-checker/affiliate-link-checker.php
+++ tools/affiliate-link-checker/affiliate-link-checker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Affiliate Link Checker
  * Description: Scant ThirstyAffiliates destination URLs op 404s en serverfouten.
- * Version: 0.2.7
+ * Version: 0.2.8
  * Update URI: https://github.com/olafdepolaf-ai/bol-affiliate-insights
  * Author: olafdepolaf-ai
  * Author URI: https://github.com/olafdepolaf-ai/bol-affiliate-insights
